7314a6e55236fa7ee8f6c7511142966f14361673
moonwalkie <moonwalkietalkie@gmail.com>
2023-09-16 21:50:18 +0200

    Add draw mode

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const sketch = document.querySelector('.sketch');
+let isDrawing = false; // Flag to track drawing state
 
+// Function to create the grid
 function createGrid(rows, columns) {
     for (let i = 0; i < rows * columns; i++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
         sketch.appendChild(cell);
 
+        // Mouseover event for coloring cells when drawing
         cell.addEventListener('mouseover', (event) => {
-            const selectedColor = document.getElementById('setcolor').value;
-            event.target.style.backgroundColor = selectedColor;
+            if (isDrawing) {
+                const selectedColor = document.getElementById('setcolor').value;
+                event.target.style.backgroundColor = selectedColor;
+            }
+        });
+
+        // Mousedown event to start drawing
+        cell.addEventListener('mousedown', () => {
+            isDrawing = true; // Press to start drawing
+        });
+
+        // Mouseup event to stop drawing
+        cell.addEventListener('mouseup', () => {
+            isDrawing = false; // Release to stop drawing
         });
     }
 }
 
 createGrid(16, 16);
 
-const colorPicker = document.getElementById('setcolor');
+const btnDraw = document.querySelector('.btn.draw');
 
-colorPicker.addEventListener('input', (event) => {
-    const selectedColor = event.target.value;
-
-    cell.style.backgroundColor = selectedColor;
+btnDraw.addEventListener('click', () => {
+    isDrawing = !isDrawing; // Toggle drawing state
+    btnDraw.classList.toggle('active');
 });
